22d4e620d5ac15f46c5ca3eaf061588928338778
digachandra <imdigachandra@gmail.com>
2016-10-18 17:44:00 +0700

    Bug fix index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs')
 app.use(expressLayouts)
 
 app.use(express.static(path.join(__dirname, 'public')))
-app.use('/', require('/routes'))
+app.use('/', require('./routes'))
 
 app.listen(port)
 console.log(`Live on port ${port}`)
