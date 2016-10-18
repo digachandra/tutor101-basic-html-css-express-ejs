ca8071ae709dbc74882d3eaa15d68666beb101d1
digachandra <imdigachandra@gmail.com>
2016-10-18 17:33:57 +0700

    Init skeleton & routes

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express'),
+      bodyParser = require('body-parser'),
+      path = require('path'),
+      ejs = require('ejs'),
+      expressLayouts = require('express-ejs-layouts')
+
+var app = express()
+var port = process.env.PORT || 9000
+
+app.set('view engine', 'ejs')
+app.use(expressLayouts)
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use('/', require('/routes'))
+
+app.listen(port)
+console.log(`Live on port ${port}`)
